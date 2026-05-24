b08e493969e204edfd9f7dbfea07d025bd419552
danryu <dan.garton@gmail.com>
2026-05-24 11:38:14 +0200

    Add MSVC build support (Windows / cl.exe)

--- src/assert.hpp
+++ src/assert.hpp
@@ -21,48 +21,28 @@
 //
 // returns automatically detected error value
 //
+// MSVC accepts `return {}` as the correct "empty" value for optional, pointers,
+// bool, and numeric return types. Void functions must use bail_v/ensure_v instead.
 
-template <comptime::String str>
-constexpr auto type_string_to_type() -> auto {
-    if constexpr(comptime::starts_with<str, "std::unique_ptr<"> ||
-                 comptime::starts_with<str, "std::shared_ptr<">) {
-        return nullptr;
-    } else if constexpr(str.str() == "void") {
-        return;
-    } else if constexpr(str.str() == "bool") {
-        return false;
-    } else if constexpr(str.str() == "int") {
-        return -1;
-    } else if constexpr(comptime::starts_with<str, "std::optional<">) {
-        return std::nullopt;
-    } else {
-        return;
+template <comptime::String sig>
+constexpr auto msft_sig_is_void_fn() -> bool {
+    if constexpr(comptime::starts_with<sig, "void __cdecl ">) {
+        return true;
     }
+    if constexpr(comptime::starts_with<sig, "void __stdcall ">) {
+        return true;
+    }
+    if constexpr(comptime::starts_with<sig, "void __fastcall ">) {
+        return true;
+    }
+    return false;
 }
 
-template <comptime::String func>
-constexpr auto detect_error_value() -> auto {
-    constexpr auto str000 = func;
-    constexpr auto str010 = comptime::remove_prefix<str000, "static ">;
-    constexpr auto str020 = comptime::remove_prefix<str010, "virtual ">;
-    constexpr auto str030 = comptime::remove_prefix<str020, "const ">;
-    constexpr auto space  = comptime::find<str030, " ">;
-    if constexpr(space == std::string_view::npos) {
-        return;
-    } else {
-        constexpr auto ret  = comptime::substr<str030, 0, space>;
-        constexpr auto name = comptime::substr<str030, space + 1>;
-        if constexpr(ret[-1] == '*' || name[0] == '*') {
-            return nullptr;
-        } else {
-            return type_string_to_type<ret>();
-        }
-    }
-}
-
-#define bail(...)                         \
-    CUTIL_MACROS_PRINT_FUNC(__VA_ARGS__); \
-    return detect_error_value<CUTIL_COMPSTR(std::source_location::current().function_name())>();
+#define bail(...)                                                                                \
+    do {                                                                                           \
+        CUTIL_MACROS_PRINT_FUNC(__VA_ARGS__);                                                      \
+        return {};                                                                                   \
+    } while(0)
 
 #define ensure(cond, ...)                                      \
     if(!(cond)) {                                              \
@@ -96,15 +76,11 @@ constexpr auto error_value = VoidErrorType{};
         bail("assertion failed" __VA_OPT__(": ") __VA_ARGS__); \
     }
 
-// returns explicitly specified variable on error
-// constexpr auto error_value = (-1, std::nullopt, nullptr, ...)
 #define bail_v(...)         generic_bail(return return_error_v(error_value), __VA_ARGS__)
 #define ensure_v(cond, ...) generic_ensure(bail_v, cond, __VA_ARGS__)
 
-// coroutine version
 #define co_bail_v(...)         generic_bail(co_return return_error_v(error_value), __VA_ARGS__)
 #define co_ensure_v(cond, ...) generic_ensure(co_bail_v, cond, __VA_ARGS__)
 
-// executes action on error
 #define bail_a(...)         generic_bail(error_act, __VA_ARGS__)
 #define ensure_a(cond, ...) generic_ensure(bail_a, cond, __VA_ARGS__)
--- src/coop-assert.hpp
+++ src/coop-assert.hpp
@@ -3,34 +3,49 @@
 
 #include "assert.hpp"
 
-template <comptime::String func>
-constexpr auto coop_detect_error_value() -> auto {
-    constexpr auto str000 = func;
-    constexpr auto str010 = comptime::remove_prefix<str000, "static ">;
-    constexpr auto str020 = comptime::remove_prefix<str010, "virtual ">;
-    constexpr auto str030 = comptime::remove_prefix<str020, "const ">;
-    constexpr auto marker = comptime::String("coop::Async<");
-    constexpr auto open   = comptime::find<str030, marker>;
-    if constexpr(open == std::string_view::npos) {
-        return;
-    } else {
-        constexpr auto close = comptime::find<str030, "> ", open + marker.size()>;
-        if constexpr(close == std::string_view::npos) {
-            return;
-        } else {
-            constexpr auto ret = comptime::substr<str030, open + marker.size(), close - open - marker.size()>;
-            if constexpr(ret[-1] == '*') {
-                return nullptr;
-            } else {
-                return type_string_to_type<ret>();
-            }
-        }
+template <comptime::String sig>
+constexpr auto msft_sig_has_trailing_void_return() -> bool {
+    if constexpr(comptime::ends_with<sig, "-> void">) {
+        return true;
     }
+    if constexpr(comptime::find<sig, "-> void "> != std::string_view::npos) {
+        return true;
+    }
+    if constexpr(comptime::find<sig, "-> void__ptr64"> != std::string_view::npos) {
+        return true;
+    }
+    return false;
 }
 
-#define coop_bail(...)                    \
-    CUTIL_MACROS_PRINT_FUNC(__VA_ARGS__); \
-    co_return coop_detect_error_value<CUTIL_COMPSTR(std::source_location::current().function_name())>();
+template <comptime::String sig>
+constexpr auto msft_sig_is_void_coop() -> bool {
+    if constexpr(comptime::find<sig, "coop::Async<void>"> != std::string_view::npos) {
+        return true;
+    }
+    if constexpr(comptime::find<sig, "coop::Async<void >"> != std::string_view::npos) {
+        return true;
+    }
+    if constexpr(comptime::find<sig, "coop::CoGenerator<void>"> != std::string_view::npos) {
+        return true;
+    }
+    if constexpr(comptime::find<sig, "coop::CoGenerator<void >"> != std::string_view::npos) {
+        return true;
+    }
+    if constexpr(msft_sig_has_trailing_void_return<sig>()) {
+        return true;
+    }
+    return msft_sig_is_void_fn<sig>();
+}
+
+#define coop_bail(...)                                                                          \
+    do {                                                                                          \
+        CUTIL_MACROS_PRINT_FUNC(__VA_ARGS__);                                                     \
+        if constexpr(msft_sig_is_void_coop<CUTIL_COMPSTR(__FUNCSIG__)>()) {                        \
+            co_return;                                                                            \
+        } else {                                                                                  \
+            co_return {};                                                                           \
+        }                                                                                         \
+    } while(0)
 
 #define coop_ensure(cond, ...)                                      \
     if(!(cond)) {                                                   \
